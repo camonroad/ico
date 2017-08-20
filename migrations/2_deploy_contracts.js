d67701b2436512a73ed3c1de88a46353b26b2d2c
Paul Rudnitskiy <me@prudnitskiy.pro>
2017-08-20 23:59:56 +0300

    pre-release final check

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,18 @@
 var CamOnRoadCrowdsale = artifacts.require("./CamOnRoadCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
-  const startBlock = web3.eth.blockNumber + 60 // blockchain block number where the crowdsale will start
-  const preIcoEndBlock = startBlock + 2880  // blockchain block number where pre ICO will end.
-  const endBlock = startBlock + 23040  // blockchain block number where crowdsale will end.
+  const startBlock = 4203697 // blockchain block number where the crowdsale will start
+  const preIcoEndBlock = 4229499  // blockchain block number where pre ICO will end.
+  const endBlock = 4355876  // blockchain block number where crowdsale will end.
   const rate = new web3.BigNumber(800) // rate of token in ETH. send X eth - get X*rate tokens
   const mininvest = new web3.BigNumber(50) // minimal pay avaliable in finney. Finney is 1/1000 ether
   const ownerWallet = "0x51cB668d545C6B68f59CEC6b988E51916355e0CA" // the address that will hold tokens and will manage contract
   const beneficiaryWallet = "0x6ED051aF9501EaBb9D231E697A2ff2C76Fb9b4fa" // multisig address that will hold money (not tokens)
-  const goal = new web3.BigNumber(100) // crowdsale goal.
+  const goal = new web3.BigNumber(10000) // crowdsale goal.
   const preIcoCap = new web3.BigNumber(1250) // crowdsale ICO cap. Max pre-ico inverstors can buy until preico ends (see preIcoEndBlock param). Currently in finney
-  const tokensTotal = new web3.BigNumber(100000) // How much token we must have at ALL.
-  const tokensForSale = new web3.BigNumber(65000) // How much token we should sale.
-  const tokensForTeam = new web3.BigNumber(25000) // How much token we'll reserve for team. token will be locked in vault.
+  const tokensTotal = new web3.BigNumber(10000000) // How much token we must have at ALL.
+  const tokensForSale = new web3.BigNumber(6500000) // How much token we should sale.
+  const tokensForTeam = new web3.BigNumber(2500000) // How much token we'll reserve for team. token will be locked in vault.
   deployer.deploy(CamOnRoadCrowdsale,
                   startBlock,
                   preIcoEndBlock,
